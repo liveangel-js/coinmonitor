d3039eea116b254bb8057ccc8da4f522ed9cf10b
liveangel <jiangshu.social@gmail.com>
2018-05-02 23:58:02 +0800

    Initial commit

---
+++ src/main/java/org/liveangel/A/PriceCraw.java
@@ -0,0 +1,21 @@
+package org.liveangel.A;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by liveangel on 2018-5-2.
+ */
+@Component
+public class PriceCraw {
+    private Logger logger = LoggerFactory.getLogger(PriceCraw.class);
+
+    @Scheduled(fixedDelayString = "1000")
+    public void test(){
+
+        logger.info("dd");
+
+    }
+}
---
+++ src/main/java/org/liveangel/OkexApplication.java
@@ -0,0 +1,12 @@
+package org.liveangel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkexApplication.class, args);
+	}
+}
